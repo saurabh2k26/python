9d76f5618abd6ea9946106a8b168ee265e08a972
saurabh2k26 <saurabh2k26@gmail.com>
2020-08-24 12:17:11 +0530

    Create array.py

---
+++ array.py
@@ -0,0 +1,11 @@
+#create new array
+my_list = []
+my_list = list()  #better
+
+#navigating thru the list
+animals = ['dog', 'cat', 'rat']
+for i in animals:
+  print(i)
+for i in range(0:len(animals):
+  print animals[i]
+  
